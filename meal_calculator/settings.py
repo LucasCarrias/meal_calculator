bb475fe9fd66a11a7598314b1f2102434aeb6b89
Lucas Carrias <lucascarrias@outlook.com>
2021-01-31 22:16:07 -0300

    Paginate responses

--- meal_calculator/settings.py
+++ meal_calculator/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'meal_calculator.urls'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
