f886a211affb160334dbab9a9e9e720dc42ff11b
Lucas Carrias <lucascarrias@outlook.com>
2021-02-01 21:54:14 -0300

    Create category views

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,105 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Category
+from chef.models import Chef
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+class CategoryAPITest(APITestCase):
+    def setUp(self):
+        for i in range(30):
+            Category.objects.create(name=f"Category {i}")
+        
+        self.chef = Chef.objects.create(username="chef", password="chef")
+        self.token = f"Bearer {str(RefreshToken.for_user(self.chef).access_token)}"
+
+        self.admin = User.objects.create(username="admin", password="admin", is_superuser=True)
+        self.admin_token = f"Bearer {str(RefreshToken.for_user(self.admin).access_token)}"
+
+    def test_get_category_list_as_admin(self):
+        url = reverse('category-list')
+
+        response = self.client.get(url, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data["results"]), 25)
+
+    def test_get_category_list_as_chef(self):
+        url = reverse('category-list')
+
+        response = self.client.get(url, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data["results"]), 25)
+
+    def test_create_category_as_admin(self):
+        url = reverse('category-list')
+
+        response = self.client.post(url, data={"name":"test"}, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_category_as_chef(self):
+        url = reverse('category-list')
+
+        response = self.client.post(url, data={"name":"test"}, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_category_as_admin(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.get(url, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_category_as_chef(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.get(url, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_put_category_as_admin(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.put(url, data={'name':'new'}, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_put_category_as_chef(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.put(url, data={'name':'new'}, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 403)
+    
+    def test_patch_category_as_admin(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.patch(url, data={'name':'new'}, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_patch_category_as_chef(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.patch(url, data={'name':'new'}, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 403)
+    
+    def test_delete_category_as_admin(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.delete(url, HTTP_AUTHORIZATION=self.admin_token)
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_category_as_chef(self):
+        url = reverse('category-detail', kwargs={'pk':1})
+
+        response = self.client.delete(url, HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 403)
+
+    
--- category/views.py
+++ category/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryListView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def post(self, request, *args, **kwargs):
+        return is_admin(request) or self.create(request, *args, **kwargs)
+
+
+class CategoryDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        return is_admin(request) or self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return is_admin(request) or self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return is_admin(request) or self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return is_admin(request) or self.destroy(request, *args, **kwargs)
+
+def is_admin(request):
+    user = request.user
+    if not user.is_superuser:
+        return Response(status=403)
+
