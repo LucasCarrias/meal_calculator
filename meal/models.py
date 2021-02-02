8a28217010d4b7ffac37dbf74d93a65d46c5f8ad
Lucas Carrias <lucascarrias@outlook.com>
2021-02-01 22:25:42 -0300

    Create meal model

--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from chef.models import Chef
+from category.models import Category
 
-# Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE, related_name='chef')
+    category = models.ManyToManyField(Category, null=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+    
--- meal/tests.py
+++ meal/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Meal
+from chef.models import Chef
+from category.models import Category
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.urls import reverse
 
-# Create your tests here.
+
+class MealAPITest(APITestCase):
+    def setUp(self):
+         Category.objects.create(name="test")
+
+        self.chef = Chef.objects.create(username="chef", password="chef")
+        self.token = f"Bearer {str(RefreshToken.for_user(self.chef).access_token)}"
+
+        for i in range(30):
+           Meal.objects.create(chef=self.chef, category=self.category, name=f"Meal {i}")
+        
+        self.admin = User.objects.create(username="admin", password="admin", is_superuser=True)
+        self.admin_token = f"Bearer {str(RefreshToken.for_user(self.admin).access_token)}"
+
+    
+    def test_get_meals_list(self):
+        url = reverse('meal-list')
+
+        response = self.client.get(url)
