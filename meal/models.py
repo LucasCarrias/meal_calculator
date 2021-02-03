3be8b6dd949f47a4bd9e7f69f283017da6735e6b
Lucas Carrias <lucascarrias@outlook.com>
2021-02-02 23:01:34 -0300

    Remove null option from Meal Many to Many field

--- meal/models.py
+++ meal/models.py
@@ -5,7 +5,7 @@ from category.models import Category
 class Meal(models.Model):
     name = models.CharField(max_length=100)
     chef = models.ForeignKey(Chef, on_delete=models.CASCADE, related_name='chef')
-    category = models.ManyToManyField(Category, null=True, blank=True, related_name='categories')
+    category = models.ManyToManyField(Category, blank=True, related_name='categories')
 
     def __str__(self):
         return self.name
